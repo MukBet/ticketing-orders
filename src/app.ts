20a838bdce9b7f93b69ba957e35dec64b307ce77
MukBet <mukbet@users.noreply.github.com>
2026-01-09 21:34:38 +0200

    init orders service

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+// This file just configures the express app and exports it for use in other files
+import express from 'express';
+import 'express-async-errors';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@motway_ticketing/common';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+import { indexOrderRouter } from './routes/index';
+import { deleteOrderRouter } from './routes/delete';
+
+const app = express();
+app.set('trust proxy', true); // trust first proxy
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',//true, // for dev only; set to true in prod with HTTPS
+  })
+);
+app.use(currentUser);
+
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
+
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
