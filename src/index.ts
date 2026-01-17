328e1ed4b0aace5cb34be9689cb5dc004a4fba03
MukBet <mukbet@users.noreply.github.com>
2026-01-17 21:21:31 +0200

    added listeners of ticket ceration and update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 
 const start = async () => {
 
@@ -38,7 +40,10 @@ const start = async () => {
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
 
-    await mongoose.connect(process.env.MONGO_URI);
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
+
+    await mongoose.connect(process.env.MONGO_URI); // В уроке 393 тут конфиг ещ' на три поля сетится
     console.log('Connected to MongoDB!');
   } catch (err) {
     console.error(err);
